2830140901992b461f6194c77f0810155d3ecf75
Rowas <white_wolf_the_white@hotmail.com>
2024-09-02 22:01:24 +0200

    Added exercise1() to Indexing. Added exercise2() to Indexing.

--- Exercises_Indexing/Program.cs
+++ Exercises_Indexing/Program.cs
@@ -1,4 +1,5 @@
-﻿//A letter on each row, user specified
+﻿/*
+//A letter on each row, user specified
 static void Exercise1()
 {
     Console.WriteLine("Enter a string:");
@@ -9,3 +10,18 @@ static void Exercise1()
     }
 }
 Exercise1();
+*/
+
+/*
+//Translate int number to string number on user request
+static void Exercise2()
+{
+    string[] numberList = new string[10] { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
+
+    Console.Write($"Enter a number: ");
+    int requestNum = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine(numberList[requestNum]);
+}
+Exercise2();
+*/
