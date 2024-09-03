e19c14ac68cd13cfbc13c4dc691f77f2c29269b1
Rowas <white_wolf_the_white@hotmail.com>
2024-09-03 10:29:37 +0200

    Added Exercise1() to Functions.

--- Exercises_Functions/Program.cs
+++ Exercises_Functions/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿/*
+//Add first and last name together and greet. 
+static void Exercise1()
+{
+    static void Greet(string fName = "", string lName = "")
+    {
+        Console.WriteLine($"Greetings {fName} {lName}");
+    }
+    Greet("Andreas", "Andersson");
+}
+Exercise1();
+*/
