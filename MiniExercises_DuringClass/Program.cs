731f486e3a2a6c1d758b51e27066c9776cbe3fc3
Rowas <white_wolf_the_white@hotmail.com>
2024-09-10 09:52:04 +0200

    ----

---
+++ MiniExercises_DuringClass/Program.cs
@@ -0,0 +1,26 @@
+﻿
+static int CalculateDiffFromNow(int futureYear)
+{
+    int currentYear = DateTime.Now.Year;
+
+    int yearDiff = futureYear - currentYear;
+
+    if (yearDiff < 1)
+    {
+        throw new ArgumentOutOfRangeException();
+    }
+
+    return yearDiff;
+}
+
+try
+{
+    int futureYear = 2019;
+    int years = CalculateDiffFromNow(futureYear);
+    Console.WriteLine($"The year {futureYear} is {years} in the future.");
+
+}
+catch (ArgumentOutOfRangeException)
+{
+    Console.WriteLine("The year is not in the future!");
+}
