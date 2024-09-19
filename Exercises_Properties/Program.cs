06aa3c1a3a6fd47853c0d7239adfc5ad671f0487
Rowas <white_wolf_the_white@hotmail.com>
2024-09-19 07:45:06 +0200

    Created Exercises_Properties. Completed Exercise 1

---
+++ Exercises_Properties/Program.cs
@@ -0,0 +1,25 @@
+﻿//Exercise1
+//Get/set _firstname
+
+Person mySelf = new Person();
+
+mySelf.FirstName = "Slim";
+
+Console.WriteLine($"Hi, my name is {mySelf.FirstName}");
+
+class Person
+{
+    private string _firstName = "Bond";
+    
+    public string FirstName
+    {
+        get
+        {
+            return _firstName;
+        }
+        set
+        {
+            _firstName = value;
+        }
+    }
+}
