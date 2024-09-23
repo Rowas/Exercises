2fecdf7e875e2befe2152314c132f39fe5859514
Rowas <white_wolf_the_white@hotmail.com>
2024-09-23 13:40:11 +0200

    Added Exercises_Inheritance. Began working on exercises.

---
+++ Exercises_Inheritance/Program.cs
@@ -0,0 +1,24 @@
+﻿using Exercise_Inheritance;
+using System.Drawing;
+
+
+Vehicle car1 = new Vehicle(Brands.Ford, Colors.Red);
+Vehicle car2 = new Vehicle(Brands.Honda);
+
+
+Console.WriteLine($"{car1.Color} {car1.Brand}");
+
+Console.WriteLine();
+
+Console.WriteLine($"{car2.Color} {car2.Brand}");
+
+Console.WriteLine();
+
+Vehicle car3 = new Vehicle(Brands.Audi, Colors.Black);
+
+Console.WriteLine(car3);
+
+Console.WriteLine(new Vehicle(Brands.Toyota, Colors.White));
+
+Console.WriteLine();
+
---
+++ Exercises_Inheritance/Vehicle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_Inheritance
+{
+    public enum Brands
+    {
+        Ford,
+        Honda,
+        Audi,
+        BMW,
+        Toyota
+    };
+    public enum Colors
+    {
+        Red,
+        Blue,
+        Green,
+        Black,
+        White
+    };
+    internal class Vehicle
+    {
+        public Brands Brand { get; set; }
+        public Colors Color { get; set; }
+
+        public Vehicle(Brands brand, Colors color)
+        {
+            Brand = brand;
+            Color = color;
+        }
+
+        public Vehicle(Brands brand)
+        {
+            Brand = brand;
+            Color = Colors.White;
+        }
+
+        public override string ToString()
+        {
+            return $"A {Color} {Brand}";
+        }
+    }
+}
