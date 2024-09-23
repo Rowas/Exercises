a2206c84ebe9c917cf6b64ec6d8f80b5284b635d
Rowas <white_wolf_the_white@hotmail.com>
2024-09-23 15:26:53 +0200

    //ToString() overide gives new default output.

---
+++ Exercises_Inheritance/Car.cs
@@ -0,0 +1,25 @@
+﻿using Exercise_Inheritance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Exercises_Inheritance
+{
+    internal class Car : Vehicle
+    {
+        public string Model { get; set; }
+
+        public Car(Brands brand, string model, Colors color) : base(brand, color)
+        {
+            Model = model;
+        }
+
+        public override string ToString()
+        {
+            return $"A {Color} {Model} from {Brand}";
+        }
+    }
+}
