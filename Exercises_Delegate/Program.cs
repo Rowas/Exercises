adc88bb96ea4e83c8702dcea76895e32561fc2ec
Rowas <white_wolf_the_white@hotmail.com>
2024-10-14 13:42:57 +0200

    Started Delegate Exercises. Started Test project for WPF

---
+++ Exercises_Delegate/Program.cs
@@ -0,0 +1,20 @@
+﻿//Exercise 1
+
+//?????????
+
+
+//Exercise 2 
+
+//?????????
+
+//Exercise 3
+
+Func<string, string, string> myFirstDelegate = FullName;
+
+Console.WriteLine(myFirstDelegate("John", "Doe"));
+
+static string FullName(string first, string last)
+{
+    return $"{first} {last}";
+}
+
