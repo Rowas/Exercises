98e180d38a9661718d1d44ffa773530fa0bfd848
Rowas <white_wolf_the_white@hotmail.com>
2024-10-15 16:04:24 +0200

    Stuffs

--- WPF_FirstApp/MainWindow.xaml.cs
+++ WPF_FirstApp/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace WPF_FirstApp
 {
@@ -11,37 +12,73 @@ namespace WPF_FirstApp
         //List<Students> studentList = new List<Students>();
         //int studentIndex = 0;
 
+        int clicks = 0;
         public MainWindow()
         {
             InitializeComponent();
         }
 
-    //    private void ConfirmButton_Click(object sender, RoutedEventArgs e)
-    //    {
-    //        Button button = (sender as Button);
-    //        studentList.Add(new Students(firstName.Text.ToString(), lastName.Text.ToString(), mailBox.Text.ToString(), studentIndex++));
-    //        studentBox.Items.Add($"{firstName.Text} {lastName.Text}");
-    //        firstName.Clear(); lastName.Clear(); mailBox.Clear();
-    //    }
-    //}
-    //class Students
-    //{
-    //    public string FirstName { get; set; }
-    //    public string LastName { get; set; }
-    //    public string Email { get; set; }
-    //    public int Index { get; set; }
 
-    //    public Students()
-    //    {
 
-    //    }
+        //private void ClickButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    ClickButton.Content = $"Clicks: {clicks++}";
+        //}
 
-    //    public Students(string firstName, string lastName, string email, int index)
-    //    {
-    //        Index = index;
-    //        FirstName = firstName;
-    //        LastName = lastName;
-    //        Email = email;
-    //    }
+
+        //private void Button_Click(object sender, RoutedEventArgs e)
+        //{
+        //    lableToChange.Content = NewLable.Text;
+        //    comboBox.Items.Add(NewLable2.Text);
+        //}
+
+        //private void NewLable2_TextChanged(object sender, TextChangedEventArgs e)
+        //{
+        //    ChangeButton.Content = NewLable2.Text;
+        //}
+
+        //private void NewLable_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        //{
+        //    if (e.Key == Key.Enter)
+        //        { lableToChange.Content = NewLable.Text; }
+        //}
+
+        //private void DisableButton_Checked(object sender, RoutedEventArgs e)
+        //{
+        //    if (DisableButton.IsChecked == true)
+        //    {
+        //        ChangeButton.IsEnabled = true;
+        //        return;
+        //    }
+        //    ChangeButton.IsEnabled = false;
+
+
+        //    private void ConfirmButton_Click(object sender, RoutedEventArgs e)
+        //    {
+        //        Button button = (sender as Button);
+        //        studentList.Add(new Students(firstName.Text.ToString(), lastName.Text.ToString(), mailBox.Text.ToString(), studentIndex++));
+        //        studentBox.Items.Add($"{firstName.Text} {lastName.Text}");
+        //        firstName.Clear(); lastName.Clear(); mailBox.Clear();
+        //    }
+        //}
+        //class Students
+        //{
+        //    public string FirstName { get; set; }
+        //    public string LastName { get; set; }
+        //    public string Email { get; set; }
+        //    public int Index { get; set; }
+
+        //    public Students()
+        //    {
+
+        //    }
+
+        //    public Students(string firstName, string lastName, string email, int index)
+        //    {
+        //        Index = index;
+        //        FirstName = firstName;
+        //        LastName = lastName;
+        //        Email = email;
+        //    }
     }
 }
